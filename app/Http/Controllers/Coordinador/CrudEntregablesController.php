3896a44c1669a00fca433751d043248759056725
LGNucamendi <LGNucamendi12@hotmail.com>
2018-06-26 14:47:17 -0600

    CONTROLLER DE CRUD EN LA VISTA DE COORDINADOR

---
+++ app/Http/Controllers/Coordinador/CrudEntregablesController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers\Coordinador;
+
+use Illuminate\Http\Request;
+use App\Models\Entregable;
+use Illuminate\Support\Facades\DB;
+
+class CrudEntregablesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+      $entregables=DB::table('catalogo_entregables')->get();
+        return view ('crudentregables/index', compact('entregables'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+          return view('crudentregables/create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $entregable = new Entregable();
+        $entregable->descripcion = $request->get('descripcion');
+        $entregable->tipo = $request->get('tipo');
+        $entregable->save();
+        return redirect('crudentregables');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+      $entregable = Entregable::find($id);
+      return view('crudentregables/edit', compact('entregable','id'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+      $entregable = Entregable::find($id);
+      $entregable->fill($request->all());
+      $entregable->save();
+      return redirect('crudentregables');//
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+      $entregable=Entregable::find($id);
+      $entregable->delete();
+      return redirect('crudentregables');
+    }
+}
---
+++ app/Http/Controllers/Coordinador/CrudLineasController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers\Coordinador;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Lineas;
+
+class CrudLineasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+      $lineas = DB::table('catalogo_lineas')->get();
+     return view('crudlineas/index', compact('lineas'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('crudlineas/create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+          $lineas = new Lineas();
+          $lineas->linea = $request->get('linea');
+          $lineas->save();
+          return redirect('crudlineas');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+      $linea = Lineas::find($id);
+      return view('crudlineas/edit', compact('linea','id'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $lineas = Lineas::find($id);
+        $lineas->linea = $request->get('linea');
+        $lineas->save();
+        return redirect('crudlineas');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+      $lineas= Lineas::find($id);
+      $lineas->delete();
+      return redirect('crudlineas');
+    }
+}
