cbabffdf22b880afcbed586da47970b502ef100c
I.S.C. Jorge Octavio Guzmán <jogs78@gmail.com>
2018-07-05 05:12:47 -0500

    Revert "Revert "DashBoard Coordinador, falta crud de programas educativos""
    This reverts commit bc1487fe312a6ef7407eddae386028e5303afb20.

--- resources/views/sistema/Coordinador.blade.php
+++ resources/views/sistema/Coordinador.blade.php
@@ -1,142 +1,179 @@
 @extends('layouts.app')
 @section('content')
-  <div class="container">
+  <div class="container" id="dashboard">
     <div class="row justify-content-left">  {{-- primer fila --}}
       <div class="col-2 bg-primary text-white">
-        <img style='float:left;clear:none;' src="https://image.flaticon.com/icons/svg/54/54551.svg"  usemap="#linea" alt="">
-          <map name="linea" id="linea">
-            <area alt="" title="" href="registrados" shape="poly" coords="13,11,499,14,498,498,14,500" />
-          </map>
-          <img id="mas" src="https://cdn.icon-icons.com/icons2/635/PNG/512/add-button_icon-icons.com_58943.png" usemap="#convocatoria" alt="" >
-            <map name="convocatoria">
-              <area alt="" title="" href="/proyecto/especial" shape="poly" coords="13,11,499,14,498,498,14,500" />
-            </map>
-            <br>
-            <br>
-          <br>
-             Proyectos registrados:{{$proyecto}}
+        <div class="row">
+          <div class="col-6" style="border: 0px;">
+            <a  style="padding: 0;" href="registrados">
+              <img src="logos/item.png" alt="">
+            </a>
+          </div>
+          <div class="col-6"  style="border: 0px;">
+            <a href="/proyecto/especial"> 
+              <img src="logos/mas.png" alt="" >
+            </a>
+          </div>          
+        </div>
+        <div class="row">
+          Proyectos registrados:{{$proyecto}} 
+        </div>
       </div>
 
       <div class="col-2 bg-primary text-white">
-        <img src="https://image.flaticon.com/icons/svg/54/54551.svg"   usemap="#map" alt="">
-          <map name="map">
-            <area alt="" title="" href="convocatoria" shape="poly" coords="13,11,499,14,498,498,14,500" />
-          </map>
-          <img id="mas" src="https://cdn.icon-icons.com/icons2/635/PNG/512/add-button_icon-icons.com_58943.png" usemap="#create" alt="" >
-            <map name="create">
-              <area alt="" title="" href="/convocatoria/create" shape="poly" coords="13,11,499,14,498,498,14,500" />
-            </map>
-        Convocatorias:  {{$convocatorias}}
+        <div class="row">
+          <div class="col-6" style="border: 0px;">
+          <a style="display:inline;" href="convocatoria">
+            <img src="logos/item.png"   usemap="#map" alt="">
+          </a>
+          </div>
+          <div class="col-6" style="border: 0px;">
+          <a href="/convocatoria/create">
+            <img src="logos/mas.png" usemap="#create" alt="" >
+          </a>
+          </div>
+        </div>
+        <div class="row">
+          Convocatorias:  {{$convocatorias}}
+        </div>        
       </div>
-
       <div class="col-2 bg-primary text-white">
-        <img src="https://image.flaticon.com/icons/svg/54/54551.svg"   usemap="#map1" alt="">
-          <map name="map1">
-            <area alt="" title="" href="crudgastos" shape="poly" coords="13,11,499,14,498,498,14,500" />
-          </map>
-          <img id="mas" src="https://cdn.icon-icons.com/icons2/635/PNG/512/add-button_icon-icons.com_58943.png" usemap="#gasto" alt="" >
-            <map name="gasto">
-              <area alt="" title="" href="/crudgastos/create" shape="poly" coords="13,11,499,14,498,498,14,500" />
-            </map>
-        Catalogo de gastos: {{$gastos}}
+        <div class="row">
+          <div class="col-6" style="border: 0px;">  
+            <a href="crudgastos">
+              <img src="logos/item.png" alt="">  
+            </a>
+          </div>
+          <div class="col-6" style="border: 0px;">
+            <a href="/crudgastos/create">
+              <img src="logos/mas.png" alt="" >              
+            </a>
+          </div>
+        </div>
+        <div class="row">
+          Catalogo de gastos: {{$gastos}}          
+        </div>      
       </div>
-
       <div class="col-2 bg-primary text-white">
-        <img src="https://image.flaticon.com/icons/svg/54/54551.svg"  usemap="#linea1" alt="">
-          <map name="linea1" id="linea">
-            <area alt="" title="" href="crudlineas" shape="poly" coords="13,11,499,14,498,498,14,500" />
-          </map>
-          <img id="mas" src="https://cdn.icon-icons.com/icons2/635/PNG/512/add-button_icon-icons.com_58943.png" usemap="#linea2" alt="" >
-            <map name="linea2">
-              <area alt="" title="" href="/crudlineas/create" shape="poly" coords="13,11,499,14,498,498,14,500" />
-            </map>
-        Lineas de investigacion:{{$lineas}}
+        <div class="row">
+          <div class="col-6" style="border: 0px;">  
+            <a href="crudlineas">
+                <img src="logos/item.png" alt="">
+            </a>
+          </div>
+          <div class="col-6" style="border: 0px;">
+            <a href="/crudlineas/create" shape="poly" coords="13,11,499,14,498,498,14,500" />
+              <img src="logos/mas.png" alt="" >
+            </a>          
+          </div>
+        </div>
+        <div class="row">
+          Lineas de investigacion:{{$lineas}}        
+        </div>
       </div>
-
       <div class="col-2 bg-primary text-white">
-        <img src="https://cdn2.iconfinder.com/data/icons/social-productivity-line-art-2/128/profile-photo-512.png" usemap="#Map0" style=" " alt="">
-          <map name="Map0">
-            <area alt="" title="Listar" href="crudusers" shape="poly" coords="13,11,499,14,498,498,14,500" />
-          </map>
-          <img id="mas" src="https://cdn.icon-icons.com/icons2/635/PNG/512/add-button_icon-icons.com_58943.png" usemap="#user" alt="" >
-            <map name="user">
-              <area alt="" title="" href="/crudusers/create" shape="poly" coords="13,11,499,14,498,498,14,500" />
-            </map>
-        Usuarios registrados: {{$count}}
+        <div class="row">
+          <div class="col-6" style="border: 0px;">  
+            <a href="crudusers">
+              <img src="logos/item.png" alt="">
+            </a>
+            </div>
+          <div class="col-6" style="border: 0px;">
+            <a title="" href="/crudusers/create">
+              <img src="logos/mas.png" alt="" >
+            </a>            
+          </div>
+        </div>
+        <div class="row">
+          Usuarios registrados: {{$count}}
+        </div> 
       </div>
     </div> {{-- primer fila --}}
 
     <div class="row justify-content-left"> {{-- segunda fila --}}
-
-
       <div class="col-3 bg-primary text-white">
-        <img src="https://image.flaticon.com/icons/svg/54/54551.svg"  usemap="#adscri" alt="">
-          <map name="adscri" id="adscri">
-            <area alt="" title="" href="crudadscripcion" shape="poly" coords="13,11,499,14,498,498,14,500" />
-          </map>
-          <img id="mas" src="https://cdn.icon-icons.com/icons2/635/PNG/512/add-button_icon-icons.com_58943.png" usemap="#ads" alt="" >
-            <map name="ads">
-              <area alt="" title="" href="/crudadscripcion/create" shape="poly" coords="13,11,499,14,498,498,14,500" />
-            </map>
-        Adscripción{{--$longitud--}}
+        <div class="row">  
+          <div class="col-6" style="border: 0px;">  
+            <a href="crudadscripcion" style="displa:inline">
+              <img src="logos/item.png" alt="">
+            </a>
+          </div>
+          <div class="col-6" style="border: 0px;">
+            <a href="/crudadscripcion/create">
+              <img src="logos/mas.png" alt="" >
+            </a>
+          </div>
+        </div>
+        <div class="row">
+          Adscripción{{--$longitud--}}        
+        </div>
       </div>
-
-
       <div class="col-3 bg-primary text-white">
-        <img src="https://image.flaticon.com/icons/svg/54/54551.svg"  usemap="#longitud" alt="">
-          <map name="longitud" id="longitud">
-            <area alt="" title="" href="crudlongitudecaracteres" shape="poly" coords="13,11,499,14,498,498,14,500" />
-          </map>
-        Partes y longitudes del protocolo{{--$longitud--}}
+        <div class="row">
+          <div class="col-6" style="border: 0px;">
+            <a href="crudlongitudecaracteres">
+              <img src="logos/item.png" alt="">
+            </a>
+          </div>
+        </div>
+        <div class="row">
+          Partes y longitudes del protocolo
+        </div>         
       </div>
-
       <div class="col-3 bg-primary text-white">
-        <img src="https://image.flaticon.com/icons/svg/54/54551.svg"  usemap="#registro" alt="">
-          <map name="registro" id="registro">
-            <area alt="" title="" href="rregistro" shape="poly" coords="13,11,499,14,498,498,14,500" />
-          </map>
-        Restricciones de registro{{--$registro--}}
+        <div class="row">  
+          <a href="rregistro">
+            <img src="logos/item.png" alt="">
+          </a>
+        </div>
+        <div class="row">
+          Restricciones de registro 
+        </div>
       </div>
-
-
-
     </div> {{-- segunda fila --}}
+    <!-- 
+          <div class="col-6" style="border: 0px;">  
+          </div>
+          <div class="col-6" style="border: 0px;">
+          </div>
+     -->
 
     <div class="row justify-content-left"> {{-- tercer fila --}}
       <div class="col-4 bg-primary text-white">
-        <img src="https://image.flaticon.com/icons/svg/54/54551.svg"  usemap="#entregable" alt="">
-          <map name="entregable" id="crudentregable">
-            <area alt="" title="" href="crudentregables" shape="poly" coords="13,11,499,14,498,498,14,500" />
-          </map>
-          <img id="mas" src="https://cdn.icon-icons.com/icons2/635/PNG/512/add-button_icon-icons.com_58943.png" alt="" usemap="#Map" />
-          <map name="Map" id="Map">
-              <area alt="" title="" href="crudentregables/create" shape="poly" coords="7,6,497,10,495,464,8,482" />
-          </map>
-
+        <div class="row">  
+          <a href="crudentregables">
+            <img src="logos/item.png" alt="">
+          </a>
+          <a href="crudentregables/create">
+            <img src="logos/mas.png" alt="">
+          </a>
+        </div>
+        <div class="row">
         Entregables:{{$entregable}}
+        </div> 
       </div>
       <div class="col-4 bg-primary text-white">
-        <img src="https://image.flaticon.com/icons/svg/54/54551.svg" usemap="#Map1" alt="">
-          <map name="Map1">
-            <area alt="" title="" href="crudareas" shape="poly" coords="13,11,499,14,498,498,14,500" />
-          </map>
-          <img id="mas" src="https://cdn.icon-icons.com/icons2/635/PNG/512/add-button_icon-icons.com_58943.png" alt="" usemap="#Map" />
-          <map name="Map" id="Map">
-              <area alt="" title="" href="crudareas/create" shape="poly" coords="7,6,497,10,495,464,8,482" />
-          </map>
-        Areas registradas:{{$catalogo}}
+        <div class="row">  
+          <a href="crudareas"><img src="logos/item.png" alt=""></a>
+          <a href="crudareas/create"><img src="logos/mas.png" alt=""></a>
+        </div>
+        <div class="row">
+          Areas registradas:{{$catalogo}}
+        </div> 
       </div>
 
       <div class="col-4 bg-primary text-white">
-        <img src="https://image.flaticon.com/icons/svg/54/54551.svg" usemap="#tipo" alt="">
-          <map name="tipo">
-            <area alt="" title="" href="crudinvestigacion" shape="poly" coords="13,11,499,14,498,498,14,500" />
-          </map>
-          <img id="mas" src="https://cdn.icon-icons.com/icons2/635/PNG/512/add-button_icon-icons.com_58943.png" alt="" usemap="#Map" />
-          <map name="Map" id="Map">
-              <area alt="" title="" href="crudinvestigacion/create" shape="poly" coords="7,6,497,10,495,464,8,482" />
-          </map>
-        Tipos de investigacion:{{$tipo}}
+        <div class="row">  
+          <a href="crudinvestigacion">
+            <img src="logos/item.png" alt="">
+          </a>
+          <a href="crudinvestigacion/create">
+            <img src="logos/mas.png" alt="">
+          </a>
+        </div>
+        <div class="row">
+          Tipos de investigacion:{{$tipo}}
+        </div> 
       </div>
     </div>{{-- tercer fila --}}
 @endsection
@@ -144,11 +181,13 @@
 <style>
 div[class^='col-']{
   border: 1px solid white;
+}
+a img{
+  width: 100%;
+}
+.row a{
+  display: inline;
+}
 
-}
-img{
-  width:40%;
-  display: block;
-}
 </style>
 @endsection
