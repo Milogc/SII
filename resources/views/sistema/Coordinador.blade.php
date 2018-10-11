efee75d74b7215f7950b0801765e3079539a34cb
Jorge Octavio Guzmán Sánchez <jogs78@gmail.com>
2018-10-10 20:01:57 -0500

    Muestra todas las etapas

--- resources/views/sistema/Coordinador.blade.php
+++ resources/views/sistema/Coordinador.blade.php
@@ -13,6 +13,27 @@
     @endif
     @include('sistema.registro')
 
+<nav>
+  <div class="nav nav-tabs" id="nav-tab" role="tablist">
+    <a class="nav-link active" id="v-pills-regi-tab" data-toggle="pill" href="#v-pills-regi" role="tab" aria-controls="v-pills-regi" aria-selected="true">REGISTRO</a>
+    <a class="nav-link" id="v-pills-eval-tab" data-toggle="pill" href="#v-pills-eval" role="tab" aria-controls="v-pills-eval" aria-selected="false">EVALUACION</a>
+    <a class="nav-link" id="v-pills-segu-tab" data-toggle="pill" href="#v-pills-segu" role="tab" aria-controls="v-pills-segu" aria-selected="false">SEGUIMIENTO</a>
+  </div>
+</nav>
+
+<div class="tab-content" id="v-pills-tabContent">
+  <div class="tab-pane fade show active" id="v-pills-regi" role="tabpanel" aria-labelledby="v-pills-regi-tab">
+    @include('sistema.registro')
+  </div>
+    
+  <div class="tab-pane fade" id="v-pills-eval" role="tabpanel" aria-labelledby="v-pills-eval-tab">
+    @include('sistema.evaluacion')
+  </div>
+  <div class="tab-pane fade" id="v-pills-segu" role="tabpanel" aria-labelledby="v-pills-segu-tab">
+    @include('sistema.seguimiento')
+  </div>
+</div>
+
 </div>
 @endsection
 @section('styles')
